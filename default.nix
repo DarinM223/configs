f2804509329021fa7e45fa3628677fd112165e70
Darin Minamoto <darinm223@gmail.com>
2018-10-29 20:12:33 -0700

    Adds more information to default.nix file

--- default.nix
+++ default.nix
@@ -1,4 +1,13 @@
+/*
+ * Note: You need to add one or more non-core dependencies to your
+ * cabal file in order for nix-shell to work (reflex-dom, text, etc)
+ */
 (import ./reflex-platform {}).project ({ pkgs, ... }: {
+  /*
+   * Uses jsaddle by default, which provides faster reloading
+   * than compiling to GHCJS. Comment this line out if you want
+   * to build to GHCJS.
+   */
   useWarp = true;
 
   packages = {
