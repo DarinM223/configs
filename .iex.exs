104e31403e00d2a239a3a690317e9cf616f4d014
Darin Minamoto <darinm223@gmail.com>
2021-03-09 09:29:51 -0800

    Clears group_history in memory to avoid having to restart IEx on every clear

--- .iex.exs
+++ .iex.exs
@@ -2,13 +2,18 @@ defmodule History do
   def write(num \\ 100, path \\ "~/.ex_history.ex") do
     {:ok, :ok} = File.open(Path.expand(path), [:write, :utf8], fn file ->
       history =
-        :lists.filter(&(not matches_history(&1)), :group_history.load)
-        |> :lists.sublist(1, num)
-      IO.write(file, :lists.reverse(history))
+        :group_history.load
+        |> Stream.filter(&(not matches_history(&1)))
+        |> Stream.take(num)
+        |> Enum.reverse
+      IO.write(file, history)
     end)
   end
 
   def clear(path \\ "~/.cache/erlang-history/") do
+    # Use `:disk_log.accessible_logs` to find out where
+    # the group_history file is.
+    :disk_log.truncate(:'$#group_history')
     {:ok, _} = File.rm_rf(Path.expand(path))
   end
 
